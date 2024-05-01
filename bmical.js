c626904e4b4891922b135978fe57b6a2225ff949
Sohail-Effendi <160319647+Sohail-Effendi@users.noreply.github.com>
2024-05-01 17:26:46 +0500

    Bmi calculation

---
+++ bmical.js
@@ -0,0 +1,4 @@
+var personWeight = 90;
+var personheight = 1.9;
+var bmical = personWeight / (personheight * personheight);
+console.log("Your bmi is ".concat(bmical));
---
+++ bmical.ts
@@ -0,0 +1,4 @@
+let personWeight : number = 90;
+let personheight : number = 1.9;
+let bmical = personWeight/(personheight*personheight);
+console.log(`Your bmi is ${bmical}`);
